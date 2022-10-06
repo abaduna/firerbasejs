13767db22027b09d8b001838f2f5fd65400fe663
abaduna <artuelrey@gmail.com>
2022-10-06 17:53:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCfIIe2WBKeXu55s_s920hPJnX5Ld4D4G0",
+
+  authDomain: "crudjs-fe063.firebaseapp.com",
+
+  projectId: "crudjs-fe063",
+
+  storageBucket: "crudjs-fe063.appspot.com",
+
+  messagingSenderId: "22669022125",
+
+  appId: "1:22669022125:web:362e53bf96c19352ba8724"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
